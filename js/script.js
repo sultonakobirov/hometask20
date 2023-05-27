e201c1c5b0c312068e34a7882808db212719661d
sultonakobirov <zafarzhanovich@gmail.com>
2023-05-27 10:29:57 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let input = prompt('Number!')
+fetch(`http://numbersapi.com/${input}/year?json`)
+  .then(response => response.json())
+  .then(data => {
+    // Обработка полученных данных
+    document.querySelector('p').textContent = data.text
+    console.log(data);
+  })
+  .catch(error => {
+    // Обработка ошибок
+    console.error('Ошибка:', error);
+  });
+
+  fetch('https://jsonplaceholder.typicode.com/posts/')
+      .then(response => response.json())
+      .then(json => console.log(json))
